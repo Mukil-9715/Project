6f083b359f10df5721846077ff8df81514133178
Mukil-9715 <mukilansuriya9715@gmail.com>
2023-06-24 16:28:45 +0530

    Login page

--- insdi/src/App.js
+++ insdi/src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
+import LoginPage from './Component/LoginPage';
+import {Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
     <div>
+      <Routes>
+        <Route path='/' element={<LoginPage/>}/>
+        {/* <Route path='/' element={<LoginPage/>}/> */}
+      </Routes>
     </div>
   );
 }
---
+++ insdi/src/Component/LoginPage.js
@@ -0,0 +1,91 @@
+import React from "react";
+import "./LoginPage.css";
+import { Col, Row } from "antd";
+import { LockOutlined, UserOutlined } from "@ant-design/icons";
+import { Button, Checkbox, Form, Input } from "antd";
+
+const LoginPage = () => {
+  const onFinish = (values) => {
+    console.log("Received values of form: ", values);
+  };
+  return (
+    <div className="">
+      {/* <Divider orientation="center">Percentage columns</Divider> */}
+      <Row className="container">
+        <Col className="left flexl" flex={2}>
+          <div className="parent brl backgroundImage ">
+            {/* <img src="https://images.pexels.com/photos/379960/pexels-photo-379960.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" width="200" />  */}
+          </div>
+        </Col>
+        <Col className="right flexr" flex={3}>
+          <div className="parent flexcolst brr">
+            <div className="th padbt">Login to INSDI </div>
+            <div className="tp padbt">Get access to your Orders, Wishlist and Recommendations</div>
+            <div>
+              <Form
+                name="normal_login"
+                className="login-forms"
+                initialValues={{
+                  remember: true,
+                }}
+                onFinish={onFinish}
+              >
+                <Form.Item
+                  name="username"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Please input your Username!",
+                    },
+                  ]}
+                >
+                  <Input
+                    prefix={<UserOutlined className="site-form-item-icon" />}
+                    placeholder="Username"
+                  />
+                </Form.Item>
+                <Form.Item
+                  name="password"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Please input your Password!",
+                    },
+                  ]}
+                >
+                  <Input.Password
+                    prefix={<LockOutlined className="site-form-item-icon" />}
+                    type="password"
+                    placeholder="Password"
+                  />
+                </Form.Item>
+                <Form.Item>
+                  <Form.Item name="remember" valuePropName="checked" noStyle>
+                    <Checkbox>Remember me</Checkbox>
+                  </Form.Item>
+
+                  <a className="login-form-forgot" href="">
+                    Forgot password
+                  </a>
+                </Form.Item>
+
+                <Form.Item>
+                  <Button
+                    type="primary"
+                    htmlType="submit"
+                    className="login-form-button"
+                  >
+                    Log in
+                  </Button>
+                  Or <Link to="/Registerform">register now!</Link>
+                </Form.Item>
+              </Form>
+            </div>
+          </div>
+        </Col>
+      </Row>
+    </div>
+  );
+};
+
+export default LoginPage;
